35af7a8d2fc2e6c13dbd07aee70f00370b2bbd3b
Elay Sason <89357293+elaysason@users.noreply.github.com>
2022-03-20 21:42:51 +0200

    Update exp3-basis.py
    2

--- exp3-basis.py
+++ exp3-basis.py
@@ -10,17 +10,22 @@ coins_list = ["Aave", "BinanceCoin", "Bitcoin", "Cardano", "ChainLink", "Cosmos"
               "Ethereum", "Iota", "Litecoin", "Monero", "NEM", "Polkadot", "Solana", "Stellar", "Tether",
               "Tron", "Uniswap", "USDCoin", "WrappedBitcoin", "XRP"]
 K = 23
+epsilon = [1 / K]
 t = 0
 T = 2991
-
+end_date = datetime.datetime.strptime("2021-07-06", "%Y-%m-%d")
 crypto_datasets = dict()
+reward_sum = dict()
 
 
-def payoff(coin, start_date,t, amountToInvest=1.0):
+def payoff(coin, start_date, t, amountToInvest=1.0):
     date = get_date(start_date, t).date()
     on_date = crypto_datasets[coin].loc[crypto_datasets[coin]["Date"] == date]
     if len(on_date) == 0:
-        return -math.inf
+        before_date = crypto_datasets[coin].loc[crypto_datasets[coin]["Date"] < date].sort_values(by=["Date"],ascending=False)
+        open = before_date['Open'].head(20).mean()
+        close = before_date['Close'].head(20).mean()
+        return amountToInvest*close/open - amountToInvest
 
     sharesBought = amountToInvest / pd.to_numeric(on_date['Open'])
     amountAfterSale = list(pd.to_numeric(sharesBought) * pd.to_numeric(on_date['Close']))[0]
@@ -54,22 +59,27 @@ def choose_coin(coin_value_list):
     probs = [float(tup[1]) for tup in coin_value_list]
     return random.choices(coins, weights=probs)[0]
 
- ## choice = random.uniform(0, sum(probs))
- ## choiceIndex = 0
 
- ## for weight in probs:
- ##     choice -= weight
- ##     if choice <= 0:
- ##         return choiceIndex
- ##     choiceIndex += 1
+## choice = random.uniform(0, sum(probs))
+## choiceIndex = 0
+
+## for weight in probs:
+##     choice -= weight
+##     if choice <= 0:
+##         return choiceIndex
+##     choiceIndex += 1
+
+def exp3_extintion(confidence_parameter, date, days):
+    epsilon = [0]
 
 
-def exp3_base(date, days, eta):
+def exp3_base(date, days):
+    eta = np.sqrt(np.log(K) / T * K)
     start_date = datetime.datetime.strptime(date, "%Y-%m-%d")
-    existing_coins = get_existing_coins(start_date, 1)
+    existing_coins = get_existing_coins(start_date, 0)
     coins_election = []
     scores = [0] * K
-    reward = lambda choice, t: payoff(coins_list[choice], start_date, t, choice)
+    reward = lambda choice, t: payoff(coins_list[choice], start_date, t)
     best_sum = 0
     reward_sum = 0
     for t in range(1, days):
@@ -82,7 +92,7 @@ def exp3_base(date, days, eta):
                 cur_distribution.append(0)
         coins_values = [(coin, cur_distribution[coin]) for coin in existing_coins]
         chosen_coin = choose_coin(coins_values)
-        cur_reward = payoff(coins_list[chosen_coin],start_date,t-1)
+        cur_reward = payoff(coins_list[chosen_coin], start_date, t=t - 1)
         for i in range(K):
             if i in existing_coins:
                 scores[i] = scores[i] + 1
@@ -90,23 +100,33 @@ def exp3_base(date, days, eta):
                     scores[i] -= (1 - cur_reward) / cur_distribution[i]
         reward_sum += cur_reward
         coins_election.append(chosen_coin)
+        best_sum += max([reward(s, t) for s in existing_coins])
         existing_coins = get_existing_coins(start_date, t)
-        best_sum += max([reward(s,t) for s in existing_coins])
 
     return best_sum - reward_sum
 
 
 if __name__ == '__main__':
     create_crypto_dict()
-    eta = np.sqrt(np.log(K) / T * K)
-    days = range(100,1000,50)
 
-    regrets = [exp3_base("2018-01-01", t, eta) for t in days]
+    ## days = range(100, 1000, 50)
+
+    ## regrets = [exp3_base("2018-01-01", t) for t in days]
+    ## print(regrets)
+    ## x = range(100, 1000, 50)
+    ## plt.title('Regret As a Function of Number of Days')
+    ## plt.plot(x, regrets)
+    ## plt.xlabel('Number Of Days')
+    ## plt.ylabel('Regret')
+    ## plt.show()
+
+    days = range(100, 2300, 150)
+
+    regrets = [exp3_base("2015-01-01", t) for t in days]
     print(regrets)
-    x = range(100,1000,50)
+    x = range(100, 2300, 150)
     plt.title('Regret As a Function of Number of Days')
-    plt.plot(x,regrets)
+    plt.plot(x, regrets)
     plt.xlabel('Number Of Days')
     plt.ylabel('Regret')
     plt.show()
-
